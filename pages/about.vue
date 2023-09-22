c68ea04721ccd350a853cba53f0b115e76bf8714
Ali_Sayed <alisayedx120008@gmail.com>
2023-09-22 12:55:34 +0300

    .....jhg

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    
+    <v-container fluid class="about portfolio-container">
+      <div id="about" class="px-4">
+      <About />
+     
+      </div>
+    </v-container>
+  </template>
+  
+  <script>
+  import About from '@/components/About.vue'
+  
+  export default {
+    components: {
+
+      About
+    }
+  }
+  </script>
+  
